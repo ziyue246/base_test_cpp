6958284404152a5cacfae3ea6cadf673e7193f8a
ziyueren <ziyueren@tencent.com>
2019-11-22 23:01:05 +0800

    Initial Commit

---
+++ base_test_cpp/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  base_test_cpp
+//
+//  Created by ziyue on 2019/11/22.
+//  Copyright © 2019 ziyue. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
